f668a6f3a92dfdcfd82bff630169d0b15151b11a
smilyte <smilyte@users.noreply.github.com>
2008-05-07 09:07:12 +0000

    Machine class

--- HoSP1_0/src/model/Machine.java
+++ HoSP1_0/src/model/Machine.java
@@ -3,7 +3,6 @@
  */
 package model;
 
-import java.util.ArrayList;
 
 /**
  * @author Elena
@@ -11,11 +10,65 @@ import java.util.ArrayList;
  */
 public class Machine {
 	
-	private String name;
+	private int serialNumber;
+	private String manufacturer;
+	private MachineType type;
 	
-	/*
-	 * Link to Machine class (0..*)
+	// TODO make connection to MachineDrawing class
+	
+	
+	
+	/**
+	 * Creates a new machine of one and only type
+	 * @param serialNumber Serial number of machine
+	 * @param manufacturer The manufacturer of machine
 	 */
-	private ArrayList<Machine> machines = new ArrayList<Machine>();
+	public Machine(int serialNumber, String manufacturer, MachineType type){
+		this.serialNumber = serialNumber;
+		this.manufacturer = manufacturer;
+		this.type = type;
+	}
+	
+	/**
+	 * Sets type of machine
+	 * Requires newType != null
+	 */
+	public void setType(MachineType newType){
+		type = newType;
+	}
+	
+	/**
+	 * @return the type
+	 */
+	public MachineType getType() {
+		return type;
+	}
 
+	/**
+	 * @return the serialNumber
+	 */
+	public int getSerialNumber() {
+		return serialNumber;
+	}
+
+	/**
+	 * @param serialNumber the serialNumber to set
+	 */
+	public void setSerialNumber(int serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	/**
+	 * @return the manufacturer
+	 */
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	/**
+	 * @param manufacturer the manufacturer to set
+	 */
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
 }
