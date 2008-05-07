a57326e23ae7f29cd3293f8a768eeba3a19b07ae
vytas.sm <vytas.sm@ec884381-8e4c-0410-83e4-b99fd916dfd4>
2008-05-07 07:58:14 +0000

    Created Machine Type class

---
+++ HoSP1_0/src/model/MachineType.java
@@ -0,0 +1,32 @@
+package model;
+
+/**
+ * 
+ * @author Vytas
+ * 
+ */
+public class MachineType {
+
+	private String name;
+
+	/*
+	 * dfdfadfadfadfsdfsdfsdfsdf
+	 */
+	
+	/**
+	 * Gets the name of the machine type.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Sets the name of the machine type.
+	 * Requirements: name <> ""
+	 * @param name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
