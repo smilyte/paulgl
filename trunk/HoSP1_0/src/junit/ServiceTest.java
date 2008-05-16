6e7aa27f59146a3516744cc72dd52f3e58ab8a32
vytas.sm <vytas.sm@ec884381-8e4c-0410-83e4-b99fd916dfd4>
2008-05-16 07:57:00 +0000

    Created new JUnit Test Case - ServiceTest

---
+++ trunk/HoSP1_0/src/junit/ServiceTest.java
@@ -0,0 +1,94 @@
+package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ServiceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetMinimumAmount() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCalculateRepairsToday() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCreateRepair() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testUpdateRepair() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDeleteRepair() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCreateRepairType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testUpdateRepairType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDeleteRepairType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCreateSparePart() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testUpdateSparePart() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDeleteSparePart() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCreateMachineType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testUpdateMachineType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDeleteMachineType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testCreateDrawer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testDeleteDrawer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
