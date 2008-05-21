99036cbe22e9056c167865878830650b86df1e34
vytas.sm <vytas.sm@ec884381-8e4c-0410-83e4-b99fd916dfd4>
2008-05-21 10:12:46 +0000

    Created DB4O Manager with all DAO classes

--- HoSP1_0/src/daoDB/DB4OManager.java
+++ HoSP1_0/src/daoDB/DB4OManager.java
@@ -1,5 +1,25 @@
 package daoDB;
 
-public interface DB4OManager {
-	//Do nothing right now. We will use it later.
+import com.db4o.Db4o;
+import com.db4o.ObjectContainer;
+
+public class DB4OManager {
+	private static DB4OManager instance = new DB4OManager();
+
+	private ObjectContainer db;
+
+	private DB4OManager() {
+	}
+
+	public static DB4OManager getInstance() {
+		return instance;
+	}
+
+	public ObjectContainer getObjectContainer() {
+		if (db == null) {
+			Db4o.configure().updateDepth(5);
+			db = Db4o.openFile("HoSP1_0.db4o");
+		}
+		return db;
+	}
 }
---
+++ HoSP1_0/src/daoDB/SparePartDAO.java
@@ -0,0 +1,54 @@
+package daoDB;
+
+import java.util.List;
+import com.db4o.ObjectContainer;
+import model.SparePart;
+
+public class SparePartDAO {
+	private static SparePartDAO instance = null;
+	
+	private ObjectContainer db = DB4OManager.getInstance().getObjectContainer();
+	
+	private SparePartDAO() {}
+	
+	/**
+	 * Return the one and only instance of the SparePartDAO class.
+	 */
+	public static SparePartDAO getInstance() {
+		if (instance == null)
+			instance = new SparePartDAO();
+		return instance;
+	}
+	
+	//.........................................................
+	/**
+	 * Adds an object of SparePart
+	 */
+	public void add(SparePart sparePart) {
+		db.set(sparePart);
+		db.commit();
+	}
+
+	/**
+	 * Deletes an object of Spare Part
+	 */
+	public void remove(SparePart sparePart) {
+		sparePart.getBox().setSp(null);
+		db.delete(sparePart);
+		db.commit();
+	}
+	/**
+	 * Returns list of spare parts
+	 */
+	public List<SparePart> getList() {
+		return db.get(SparePart.class);
+	}
+	
+	/**
+	 * Updates an object of Spare Part
+	 */
+	public void update(SparePart sparePart){
+		// nothing to do.
+
+	}
+}
