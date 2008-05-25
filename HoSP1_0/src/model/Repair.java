129f7d9e9b4545b4f3fb9ea3c7a54d5453d01f94
vytas.sm <vytas.sm@ec884381-8e4c-0410-83e4-b99fd916dfd4>
2008-05-25 18:23:09 +0000

    Modified Repair class: Added/Edited comments

--- HoSP1_0/src/model/Repair.java
+++ HoSP1_0/src/model/Repair.java
@@ -4,83 +4,89 @@ import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.List;
 
-/**
- * @author Malik
- * 
- */
 public class Repair {
-	private int num;
+	private int number;
 	private GregorianCalendar startDate;
 	private GregorianCalendar endDate;
+
 	// link to [Machine] Class(0..* --> 1)
 	private Machine machine;
+
 	// link to [PartUsage] Class(1 --> 0..*)
-	private List<PartUsage> partsUsage = new ArrayList<PartUsage>();
+	private List<PartUsage> partUsages = new ArrayList<PartUsage>();
+
 	// link to [RepairType] Class(1 --> 0..1)
 	private RepairType repairType = null;
 
 	/**
-	 * Constructor of Repair
+	 * <b>Constructor: </b> Creates of Repair
+	 * <p>
+	 * <b>Requires: </b> startDate != null, endDate != null, machine != null
+	 * <p>
 	 * 
 	 * @param num
 	 * @param startDate
 	 * @param endDate
 	 * @param machine
-	 * @param
 	 */
-	public Repair(int num, GregorianCalendar startDate,
+	public Repair(int number, GregorianCalendar startDate,
 			GregorianCalendar endDate, Machine machine) {
-		this.num = num;
+		this.number = number;
 		this.startDate = startDate;
 		this.endDate = endDate;
 		this.machine = machine;
 	}
 
+	/**
+	 * @return number of repair
+	 */
 	public int getNum() {
-		return num;
+		return number;
 	}
 
-	public void setNum(int num) {
-		this.num = num;
+	/**
+	 * Sets number
+	 * <p>
+	 * <b>Requires:</b> number > 0
+	 */
+	public void setNum(int number) {
+		this.number = number;
 	}
+
+	/**
+	 * @return start date of this repair
+	 */
 
 	public GregorianCalendar getStartDate() {
 		return startDate;
 	}
 
+	/**
+	 * Sets start date to this repair
+	 * <p>
+	 * <b>Requires: </b> startDate != null
+	 */
 	public void setStartDate(GregorianCalendar startDate) {
 		this.startDate = startDate;
 	}
 
 	/**
-	 * @return Date
+	 * @return end date of this repair
 	 */
 	public GregorianCalendar getEndDate() {
 		return endDate;
 	}
 
+	/**
+	 * Sets end date to this repair
+	 * <p>
+	 * <b>Requires: </b> endDate != null
+	 */
 	public void setEndDate(GregorianCalendar endDate) {
 		this.endDate = endDate;
 	}
 
-	/**
-	 * Adds a partUsage to this repair 
-	 * Requires: partUsage != null
-	 * 
-	 */
-	void addPartUsage(PartUsage partUsage) {
-		partsUsage.add(partUsage);
-	}
-
-	/**
-	 * Moves a partUsage from this repair to an other repair
-	 * Requires: group != null
-	 */
-	public void movePartUsage(PartUsage partUsage, Repair repair) {
-		partsUsage.remove(partUsage);
-		repair.addPartUsage(partUsage);
-	}
-
+	// .................Machine..Start...................
 	/**
 	 * @return the machine
 	 */
@@ -88,21 +94,47 @@ public class Repair {
 		return machine;
 	}
 
+	// .................Machine..End.....................
+
+	// .................Part Usage..Start................
 	/**
-	 * @return the partsUsage
+	 * Adds a part usage to this repair
+	 * <p>
+	 * <b>Requires: </b> partUsage != null
+	 * 
 	 */
-	public List<PartUsage> getPartsUsage() {
-		return partsUsage;
+	public void addPartUsage(PartUsage partUsage) {
+		partUsages.add(partUsage);
+	}
+
+	/**
+	 * Moves a part usage from this repair to an other repair
+	 * <p>
+	 * <b>Requires: </b> repair != null
+	 */
+	public void movePartUsage(PartUsage partUsage, Repair repair) {
+		partUsages.remove(partUsage);
+		repair.addPartUsage(partUsage);
+	}
+
+	/**
+	 * @return the list of all part usages of this repair
+	 */
+	public List<PartUsage> getPartUsages() {
+		return partUsages;
 	}
 
 	/**
 	 * @param partsUsage
 	 *            the partsUsage to set
 	 */
-	public void setPartsUsage(List<PartUsage> partsUsage) {
-		this.partsUsage = partsUsage;
+	public void setPartUsages(List<PartUsage> partUsages) {
+		this.partUsages = partUsages;
 	}
 
+	// .................Part Usage..End..................
+
+	// .................Repair Type..Start...............
 	/**
 	 * @return the repairType
 	 */
@@ -118,31 +150,47 @@ public class Repair {
 		this.repairType = repairType;
 	}
 
-	
+	// .................Repair Type..End.................
+
+	/**
+	 * @return down time of this repair.
+	 *         <p>
+	 *         Format: XX days XX hours XX minutes
+	 * 
+	 * @author Malik
+	 */
 	public String getDowntime() {
-		long time; 
-		time = endDate.getTimeInMillis() - startDate.getTimeInMillis();  //Gets the difference between the dates
-		long days = time/(1000*60*60*24);                  //Converts the difference to days
-		long hours = (time%(1000*60*60*24))/(1000*60*60);  //Converts the difference to hours
-		long minutes = ((time%(1000*60*60*24))%(1000*60*60))/(1000*60); //Converts the difference to minutes
-		if (days == 0 && hours == 0 && minutes == 0) return "None."; //Filter 1: If the difference is equal to 0 program return "None"
-		else if (days == 0 && hours == 0) return minutes+"minutes."; //Filter 2: If days and hours are equal to 0 than return "minutes"
-		else if (days == 0) return hours+" hours, "+minutes+"minutes.";//Filter 3: If days are equal to 0 than return "hours" and "minutes"
-		else return days+" days, "+hours+" hours, "+minutes+"minutes.";//Filter 4: If all atributes has value return "days", "hours" and "minutes"
+		long time;
+		// Gets the difference between the dates
+		time = endDate.getTimeInMillis() - startDate.getTimeInMillis();
+		// Converts the difference to days
+		long days = time / (1000 * 60 * 60 * 24);
+		// Converts the difference to hours
+		long hours = (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60);
+		// Converts the difference to minutes
+		long minutes = ((time % (1000 * 60 * 60 * 24)) % (1000 * 60 * 60))
+				/ (1000 * 60);
+		// Filter 1: If the difference is equal to 0 program return "None"
+		if (days == 0 && hours == 0 && minutes == 0)
+			return "None.";
+		// Filter 2: If days and hours are equal to 0 than return "minutes"
+		else if (days == 0 && hours == 0)
+			return minutes + "minutes.";
+		// Filter 3: If days are equal to 0 than return "hours" and "minutes"
+		else if (days == 0)
+			return hours + " hours, " + minutes + "minutes.";
+		// Filter 4: If all attributes have value return "days", "hours" and
+		// "minutes"
+		else
+			return days + " days, " + hours + " hours, " + minutes + "minutes.";
 	}
 
+	/**
+	 * @return number of repair, repair's Start and End time as string.
+	 */
 	public String toString() {
-		if(endDate != null)
-			return num + ": Started: [" + startDate.getTime() + "] Ended: [" + endDate.getTime()+"]";
-		else {
-//			String s = num+ ": Started: ["+startDate.get(GregorianCalendar.YEAR)+
-//			"."+startDate.get(GregorianCalendar.MONTH)+"."+startDate.get(GregorianCalendar.DAY_OF_MONTH)+
-//			", "+startDate.get(GregorianCalendar.DAY_OF_WEEK)+", "+startDate.get(GregorianCalendar.HOUR_OF_DAY)+
-//			":"+startDate.get(GregorianCalendar.MINUTE)+"]";
-//			return s;
-			return num + ": Started: [" + startDate.getTime() + "]";
-		}
-			
+		return number + ": Started: [" + startDate.getTime() + "] Ended: ["
+				+ endDate.getTime() + "]";
 	}
 
 }
